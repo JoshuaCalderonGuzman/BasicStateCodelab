c72411a5e9300ac653c08a9643963a0c6cedffe3
Joshua Calderon Guzman <s22120150@alumnos.itsur.edu.mx>
2025-09-18 21:47:19 -0600

    Creamos un archivo llamado WellnessViewModel y movemos el getWellnessTasks,Definimos una variable interna _tasks  y expon tasks  como lista de modo que no pueda modificarse desde fuera del ViewModel, implementamos la función remove simple que delegue a la función integrada de eliminación de lista, En app/build.gradle.kts agregamos la biblioteca para el viewmodel compose, en e; aechivo  WellnessScreen llamamos a viewModel() para crear una instancia del ViewModel y proporcionamos WellnessTasksList  a la lista de tareas quitamos la función lambda onCloseTask, después modificamos la clase  WellnessTask  para que almacene el estado de marcado, En el ViewModel implementamos el método changeTaskChecked  que recibe una tarea que se modificara con un valor nuevo para el estado marcado, En WellnessScreen llamamos al método changeTaskChecked para proporcionar el comportamiento de onCheckedTask  de la lista. Abrimos WellnessTasksList y agregamos el parámetro de la función lambda onCheckedTask  para que podamos pasarlo a WellnessTaskItem,Cambiamos el WellnessTask para que reciba una variable initialChecked con el valor predeterminado en falso y inicializar la variable checked  con el método de fabrica mutableStateOf y tomar initialChecked como valor predeterminado.

--- app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
@@ -1,5 +1,9 @@
 package com.example.basicstatecodelab
-
-data class WellnessTask(
-    val id: Int,
-    val label: String)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+data class WellnessTask(val id: Int,
+                        val label: String,
+                        val initialChecked: Boolean = false) {
+    var checked by mutableStateOf(initialChecked)
+}
